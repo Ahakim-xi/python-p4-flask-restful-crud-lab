bb8c27d9c744de2eab43e4c0de41c8e218e4acf0
Ahakim-xi <ahakimiii398@gmail.com>
2025-09-28 01:01:47 +0300

    complete lab

--- server/seed.py
+++ server/seed.py
@@ -1,28 +1,17 @@
-#!/usr/bin/env python3
 
-from app import app
-from models import db, Plant
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
+db = SQLAlchemy()
 
-with app.app_context():
+class Plant(db.Model, SerializerMixin):
+    __tablename__ = 'plants'
 
-    Plant.query.delete()
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+    is_in_stock = db.Column(db.Boolean)
 
-    aloe = Plant(
-        id=1,
-        name="Aloe",
-        image="./images/aloe.jpg",
-        price=11.50,
-        is_in_stock=True,
-    )
-
-    zz_plant = Plant(
-        id=2,
-        name="ZZ Plant",
-        image="./images/zz-plant.jpg",
-        price=25.98,
-        is_in_stock=False,
-    )
-
-    db.session.add_all([aloe, zz_plant])
-    db.session.commit()
+    def __repr__(self):
+        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
